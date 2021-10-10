02f615b821eee283676cfd24d4411d69d011e2a7
mumukiresuelto <91298052+mumukiresuelto@users.noreply.github.com>
2021-10-10 08:19:40 -0300

    commit message

---
+++ README.md
@@ -0,0 +1,9 @@
+# MumukiResuelto
+
+![MumukiResuelto Logo](https://raw.githubusercontent.com/mumukiresuelto/completo/master/images/Banner.png)
+
+Si estás buscando ir a la **web**, tenés que dirigirte al siguiente link:
+
+[mumukiresuelto.github.io/completo](https://mumukiresuelto.github.io/completo)
+
+¡Saludos! 🙋‍♂️
---
+++ js/main.js
@@ -0,0 +1,77 @@
+function main() {
+	document.getElementById("muestreoCap1Container").onclick = function() {
+		window.open("images/Capitulo1.png", "_blank").focus()
+	}
+	document.getElementById("muestreoCap2Container").onclick = function() {
+		window.open("images/Capitulo2.png", "_blank").focus()
+	}
+	document.getElementById("muestreoCap3Container").onclick = function() {
+		window.open("images/Capitulo3.png", "_blank").focus()
+	}
+	document.getElementById("muestreoCap4Container").onclick = function() {
+		window.open("images/Capitulo4.png", "_blank").focus()
+	}
+	document.getElementById("muestreoCap5Container").onclick = function() {
+		window.open("images/Capitulo5.png", "_blank").focus()
+	}
+	document.getElementById("muestreoCap6Container").onclick = function() {
+		window.open("images/Capitulo6.png", "_blank").focus()
+	}
+	document.getElementById("muestreoEj1Container").onclick = function() {
+		window.open("images/Ejercicio1.png", "_blank").focus()
+	}
+	document.getElementById("muestreoEj2Container").onclick = function() {
+		window.open("images/Ejercicio2.png", "_blank").focus()
+	}
+	document.getElementById("muestreoEj3Container").onclick = function() {
+		window.open("images/Ejercicio3.png", "_blank").focus()
+	}
+	document.getElementById("muestreoEj4Container").onclick = function() {
+		window.open("images/Ejercicio4.png", "_blank").focus()
+	}
+	document.getElementById("muestreoEj5Container").onclick = function() {
+		window.open("images/Ejercicio5.png", "_blank").focus()
+	}
+	document.getElementById("muestreoEj6Container").onclick = function() {
+		window.open("images/Ejercicio6.png", "_blank").focus()
+	}
+
+	/* ========================================================== */
+
+	document.getElementById("contactoTelegramContainerMenor").onclick = function() {
+		window.open("https://t.me/DiegoIngeniero", "_blank").focus()
+	}
+
+	/* ========================================================== */
+
+	document.getElementById("versionDescripcion").onclick = function() {
+		window.open("https://github.com/mumukiresuelto/completo/blob/master/version_history.txt", "_blank").focus()
+	}
+
+	/* ========================================================== */
+
+	document.getElementById("redEmail").onclick = function() {
+		window.open("mailto:?subject=MumukiResuelto: ¡Todos los ejercicios de la plataforma completos!&body=https://mumukiresuelto.github.io/completo", "_blank").focus()
+	}
+	document.getElementById("redFacebook").onclick = function() {
+		window.open("https://www.facebook.com/sharer/sharer.php?u=https://mumukiresuelto.github.io/completo&title=MumukiResuelto - Todos los ejercicios de la plataforma completos.", "_blank").focus()
+	}
+	document.getElementById("redLinkedin").onclick = function() {
+		window.open("https://www.linkedin.com/shareArticle?mini=true&url=https://mumukiresuelto.github.io/completo&title=MumukiResuelto: ¡Todos los ejercicios de la plataforma completos!", "_blank").focus()
+	}
+	document.getElementById("redReddit").onclick = function() {
+		window.open("https://www.reddit.com/submit?url=https://mumukiresuelto.github.io/completo&title=MumukiResuelto: ¡Todos los ejercicios de la plataforma completos!", "_blank").focus()
+	}
+	document.getElementById("redTelegram").onclick = function() {
+		window.open("https://t.me/share/url?url=https://mumukiresuelto.github.io/completo&text=**MumukiResuelto** 🇦🇷 __¡Todos los ejercicios de la plataforma completos!__", "_blank").focus()
+	}
+	document.getElementById("redTumblr").onclick = function() {
+		window.open("https://www.tumblr.com/share?v=3&u=https://mumukiresuelto.github.io/completo&t=MumukiResuelto: ¡Todos los ejercicios de la plataforma completos!", "_blank").focus()
+	}
+	document.getElementById("redTwitter").onclick = function() {
+		window.open("https://twitter.com/intent/tweet?url=https://mumukiresuelto.github.io/completo&text=MumukiResuelto 🇦🇷 ¡Todos los ejercicios de la plataforma completos!&via=MumukiResuelto&hashtags=Mumuki,MumukiResuelto,ArgentinaPrograma,SéProgramar,YoProgramo", "_blank").focus()
+	}
+	document.getElementById("redWhatsapp").onclick = function() {
+		window.open("https://wa.me/?text=*MumukiResuelto*: _¡Todos los ejercicios de la plataforma completos!_ - https://mumukiresuelto.github.io/completo", "_blank").focus()
+	}
+}
